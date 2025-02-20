ca99864499280c9ed6eea89d7253f2814d37eaf6
aruzhanmadelkhanova <madelxanova@mail.ru>
2025-02-20 19:37:20 +0500

    first task

--- Module 4/Homework/src/easy/Main.java
+++ Module 4/Homework/src/easy/Main.java
@@ -1,9 +1,12 @@
 package easy;
 
+import java.util.Scanner;
+
 public class Main {
-
-  public static void main(String[] args) {
-
-  }
-
+    public static void main(String[] args) {
+        StringPrinter stringPrinter = new StringPrinter();
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+        stringPrinter.print(word);
+    }
 }
---
+++ Module 4/Homework/src/easy/StringPrinter.java
@@ -0,0 +1,8 @@
+package easy;
+
+public class StringPrinter implements Printer{
+    @Override
+    public void print(Object value) {
+        System.out.println(value);
+    }
+}
