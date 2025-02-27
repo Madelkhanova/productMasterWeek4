b96e05814233858847a4a4ff45e1168e8ea1b935
aruzhanmadelkhanova <madelxanova@mail.ru>
2025-02-27 22:45:58 +0500

    task medium

--- Module 5/Homework/src/five/medium/Main.java
+++ Module 5/Homework/src/five/medium/Main.java
@@ -3,10 +3,21 @@ package five.medium;
 public class Main {
 
   public static void main(String[] args) {
+    ArrayList<DayOfWeek> daysOfWeek = new ArrayList<>();
+    for (DayOfWeek day : DayOfWeek.values()) {
+      daysOfWeek.add(day);
+    }
+
+    for (DayOfWeek day : daysOfWeek) {
+      System.out.println(day);
+    }
+
+    DayOfWeek dayToCheck = DayOfWeek.SUNDAY;
+    System.out.println(dayToCheck + " является выходным? " + isWeekend(dayToCheck));
 
   }
   public static boolean isWeekend(DayOfWeek day) {
-    return false;
+    return day == DayOfWeek.SATURDAY || day == DayOfWeek.SUNDAY;
   }
 
 }
