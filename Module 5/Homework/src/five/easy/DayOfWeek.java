a071bdb6e1e63abb68aa2e1fad0f40c2b2be853f
aruzhanmadelkhanova <madelxanova@mail.ru>
2025-02-27 22:35:59 +0500

    task easy

--- Module 5/Homework/src/five/easy/DayOfWeek.java
+++ Module 5/Homework/src/five/easy/DayOfWeek.java
@@ -1,4 +1,11 @@
 package five.easy;
 
 public enum DayOfWeek {
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY,
+    SUNDAY
 }
---
+++ Module 5/Homework/src/five/easy/Main.java
@@ -0,0 +1,11 @@
+package easy;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        DayOfWeek today = DayOfWeek.MONDAY;
+
+        System.out.println("Сегодня: " + today);
+    }
+}
