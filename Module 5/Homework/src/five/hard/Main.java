ebb69bf59281ed485b3999a70a49848e0c960244
aruzhanmadelkhanova <madelxanova@mail.ru>
2025-02-27 22:55:14 +0500

    task hard

--- Module 5/Homework/src/five/hard/Main.java
+++ Module 5/Homework/src/five/hard/Main.java
@@ -4,11 +4,35 @@ import java.util.ArrayList;
 
 public class Main {
 
-  public static void main(String[] args) {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
 
-  }
+        ArrayList<Integer> numbers = new ArrayList<>();
 
-  public static ArrayList<Integer> removeDuplicates(ArrayList<Integer> list) {
-    return null;
-  }
+        for (int i = 0; i < n; i++) {
+            numbers.add(scanner.nextInt());
+        }
+
+        ArrayList<Integer> uniqueNumbers = removeDuplicates(numbers);
+        for (Integer number : uniqueNumbers) {
+            System.out.print(number + " ");
+        }
+    }
+
+    public static ArrayList<Integer> removeDuplicates(ArrayList<Integer> list) {
+
+        ArrayList<Integer> result = new ArrayList<>();
+        HashSet<Integer> seen = new HashSet<>();
+
+        for (Integer number : list) {
+            if (!seen.contains(number)) {
+                result.add(number);
+                seen.add(number);
+            }
+        }
+
+        return result;
+
+    }
 }
